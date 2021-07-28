37abb040ce9d597676f3160bf93b2af0a9dcdc3f
Rodrigo Grzegorczyk <rodrigo.gregorio1@hotmail.com>
2021-07-28 11:40:40 -0300

    Send Email

--- src/models/SurveyUser.ts
+++ src/models/SurveyUser.ts
@@ -1,5 +1,7 @@
-import { Column, Entity, PrimaryColumn, CreateDateColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn, CreateDateColumn, ManyToOne, JoinColumn } from "typeorm";
 import { v4 as uuidv4 } from 'uuid';
+import { Survey } from "./Survey";
+import { User } from './User';
 
 @Entity("surveys_users")
 class SurveyUser {
@@ -9,8 +11,16 @@ class SurveyUser {
     @Column()
     user_id: string;
 
+    @ManyToOne(() => User)
+    @JoinColumn({name: "user_id"})
+    user: User;
+
     @Column()
     survey_id: string;
+
+    @ManyToOne(() => Survey)
+    @JoinColumn({name: "survey_id"})
+    survey: Survey;
 
     @Column()
     value: number;
